ebc64daa51606ac5f94e95f05b1823714ff37b41
jlangridge <l@ngridge.com>
2023-11-18 17:51:54 +0100

    Refactor logic into hooks

--- app/button.tsx
+++ app/button.tsx
@@ -1,58 +1,12 @@
 'use client'
 
-import {useEffect, useState} from 'react'
 import {clsx} from 'clsx'
-import {useLocalStorage} from 'usehooks-ts'
-
-type Count = {
-  hours: number
-  minutes: number
-  seconds: number
-}
+import {useCount} from '@/app/useCount'
+import {useIsClient} from 'usehooks-ts'
 
 export default function Button() {
-  const [count, setCount] = useLocalStorage<Count>('count', {
-    hours: 0,
-    minutes: 0,
-    seconds: 0,
-  })
-  const [isClockedIn, setIsClockedIn] = useState<boolean>(false)
-  const [isClient, setIsClient] = useState(false)
-  const {hours, minutes, seconds} = count
-
-  useEffect(() => {
-    setIsClient(true)
-  }, [])
-
-  useEffect(() => {
-    let intervalId: NodeJS.Timeout
-
-    if (isClockedIn) {
-      intervalId = setInterval(() => {
-        setCount(prev => {
-          const {hours, minutes, seconds} = prev
-
-          if (seconds === 60) {
-            if (minutes < 60) {
-              return {...prev, minutes: minutes + 1, seconds: 0}
-            }
-
-            if (minutes === 60) {
-              return {hours: hours + 1, minutes: 0, seconds: 0}
-            }
-          }
-
-          return {...prev, seconds: seconds + 1}
-        })
-      }, 1000)
-    }
-
-    return () => clearInterval(intervalId)
-  }, [isClockedIn])
-
-  function toggleCounter() {
-    setIsClockedIn(prev => !prev)
-  }
+  const {hours, isClockedIn, minutes, seconds, toggleCounter} = useCount()
+  const isClient = useIsClient()
 
   if (!isClient) {
     return null
---
+++ app/useCount.ts
@@ -0,0 +1,50 @@
+import {useLocalStorage} from 'usehooks-ts'
+import {useEffect, useState} from 'react'
+
+type Count = {
+  hours: number
+  minutes: number
+  seconds: number
+}
+
+export function useCount() {
+  const [count, setCount] = useLocalStorage<Count>('count', {
+    hours: 0,
+    minutes: 0,
+    seconds: 0,
+  })
+  const [isClockedIn, setIsClockedIn] = useState<boolean>(false)
+  const {hours, minutes, seconds} = count
+
+  useEffect(() => {
+    let intervalId: NodeJS.Timeout
+
+    if (isClockedIn) {
+      intervalId = setInterval(() => {
+        setCount(prev => {
+          const {hours, minutes, seconds} = prev
+
+          if (seconds === 60) {
+            if (minutes < 60) {
+              return {...prev, minutes: minutes + 1, seconds: 0}
+            }
+
+            if (minutes === 60) {
+              return {hours: hours + 1, minutes: 0, seconds: 0}
+            }
+          }
+
+          return {...prev, seconds: seconds + 1}
+        })
+      }, 1000)
+    }
+
+    return () => clearInterval(intervalId)
+  }, [isClockedIn])
+
+  function toggleCounter() {
+    setIsClockedIn(prev => !prev)
+  }
+
+  return {hours, isClockedIn, minutes, seconds, toggleCounter}
+}
