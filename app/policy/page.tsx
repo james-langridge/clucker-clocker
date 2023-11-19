16384ea58a7660447aef9d413ec08318dd3bf748
jlangridge <l@ngridge.com>
2023-11-19 11:48:46 +0100

    Update policy

--- app/policy/page.tsx
+++ app/policy/page.tsx
@@ -16,6 +16,10 @@ export default function PolicyPage() {
       <section>
         <h2 className="text-xl font-bold">Privacy Policy</h2>
         <p>
+          This site uses JSON Web Tokens and an in-memory database which resets
+          every ~2 hours.
+        </p>
+        <p>
           Data provided to this site is exclusively used to support signing in
           and is not passed to any third party services, other than via SMTP or
           OAuth for the purposes of authentication.
