b87a79bc9299bb61140ccb07ff0b14e8b56db94f
jlangridge <l@ngridge.com>
2024-01-21 21:29:36 +0100

    Replace loader with logo image

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,7 +1,6 @@
+import Image from 'next/image'
 import React from 'react'
 
-import Loader from '@/components/loader'
-
 export default function Loading() {
-  return <Loader />
+  return <Image src="/logo.png" alt="logo" width={473} height={611} />
 }
