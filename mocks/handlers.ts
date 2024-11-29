c6181022c7b3fe5198ba83872e418ac732b8a6f3
jlangridge <l@ngridge.com>
2024-11-29 15:10:39 +0100

    Fix msw handlers

--- mocks/handlers.ts
+++ mocks/handlers.ts
@@ -12,4 +12,22 @@ export const handlers = [
   http.post('/api/clock-out', () => {
     return HttpResponse.json({isClockedIn: false})
   }),
+
+  http.get('/api/clocked-times', () => {
+    return HttpResponse.json({
+      data: {
+        id: 1,
+        start: '2024-11-29T13:00:00.000Z',
+        end: '2024-11-29T17:00:00.000Z',
+      },
+    })
+  }),
+
+  http.post('/api/clocked-times', async ({request}) => {
+    const body = (await request.json()) as {start: string; end: string}
+    return HttpResponse.json({
+      id: Math.random(),
+      ...body,
+    })
+  }),
 ]
